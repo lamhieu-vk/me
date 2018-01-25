945f8216c449f96b3706f2875170fd997db37b8d
Lam Hieu <lamhieu.vk@gmail.com>
2018-01-25 08:37:40 +0700

    add cname file

--- static.config.js
+++ static.config.js
@@ -1,6 +1,3 @@
-// flow
-import React from 'react'
-
 export default {
   getSiteProps: () => ({
     title: 'lamhieu'
@@ -14,5 +11,12 @@ export default {
       is404: true,
       component: 'src/containers/Home'
     }
-  ]
+  ],
+  siteRoot: 'https://lamhieu.info',
+  paths: {
+    src: 'src', // The source directory. Must include an index.js entry file.
+    dist: 'build', // The production output directory.
+    devDist: 'dist', // The development scratch directory.
+    public: 'public' // The public directory (files copied to dist during build)
+  },
 }
