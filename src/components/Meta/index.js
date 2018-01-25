1793964f6038220e89ca72a62954d377920ad9c6
Lam Hieu <lamhieu.vk@gmail.com>
2018-01-25 09:28:12 +0700

    add key to meta tag, add gtag script

--- src/components/Meta/index.js
+++ src/components/Meta/index.js
@@ -6,25 +6,25 @@ import Helmet from 'react-helmet'
 const Meta = ({ title, keywords, image, description, favicon, links }: any) => (
   <Helmet>
     {links}
-    <meta charSet="utf-8" />
-    <meta property="keywords" content={keywords} />
-    <meta itemProp="image" content={image} />
-    <meta itemProp="description" content={description} />
-    <meta itemProp="name" content={title} />
-    <meta name="twitter:image" content={image} />
-    <meta name="twitter:description" content={description} />
-    <meta name="twitter:title" content={title} />
-    <meta name="twitter:card" content="summary" />
-    <meta property="og:description" content={description} />
-    <meta property="og:image" content={image} />
-    <meta property="og:title" content={title} />
-    <meta property="og:type" content="website" />
-    <meta name="googlebot" content="index,follow" />
-    <meta name="robots" content="index,follow" />
-    <meta name="subject" />
-    <meta name="description" content={description} />
-    <link rel="icon" type="image/png" href={favicon['32x32']} sizes="32x32" />
-    <link rel="icon" type="image/png" href={favicon['16x16']} sizes="16x16" />
+    <meta key="charset" charSet="utf-8" />
+    <meta key="keywords" property="keywords" content={keywords} />
+    <meta key="image" itemProp="image" content={image} />
+    <meta key="description" itemProp="description" content={description} />
+    <meta key="name" itemProp="name" content={title} />
+    <meta key="twitter:image" name="twitter:image" content={image} />
+    <meta key="twitter:description" name="twitter:description" content={description} />
+    <meta key="twitter:title" name="twitter:title" content={title} />
+    <meta key="twitter:card" name="twitter:card" content="summary" />
+    <meta key="og:description" property="og:description" content={description} />
+    <meta key="og:image" property="og:image" content={image} />
+    <meta key="og:title" property="og:title" content={title} />
+    <meta key="og:type" property="og:type" content="website" />
+    <meta key="googlebot" name="googlebot" content="index,follow" />
+    <meta key="robots" name="robots" content="index,follow" />
+    <meta key="subject" name="subject" />
+    <meta key="description" name="description" content={description} />
+    <link key="icon32x32" rel="icon" type="image/png" href={favicon['32x32']} sizes="32x32" />
+    <link key="icon16x16" rel="icon" type="image/png" href={favicon['16x16']} sizes="16x16" />
     <title>{title}</title>
   </Helmet>
 )
--- static.config.js
+++ static.config.js
@@ -1,3 +1,14 @@
+// @flow
+
+import React from 'react'
+
+const gtagScript = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-108475565-1');
+`
+
 export default {
   getSiteProps: () => ({
     title: 'lamhieu'
@@ -19,4 +30,17 @@ export default {
     devDist: 'dist', // The development scratch directory.
     public: 'public' // The public directory (files copied to dist during build)
   },
+  Document: ({ Html, Head, Body, children }) => (
+    <Html lang="en-US">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-108475565-1"
+        />
+        <script>{gtagScript}</script>
+      </Head>
+      <Body>{children}</Body>
+    </Html>
+  )
 }
