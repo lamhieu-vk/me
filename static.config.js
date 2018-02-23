6e9963769b18c5b186dcbd8cc9a8e2b2857ebc00
Lam Hieu <lamhieu.vk@gmail.com>
2018-02-23 13:54:35 +0700

    update static config

--- static.config.js
+++ static.config.js
@@ -15,17 +15,22 @@ const data = {
     'https://maxcdn.icons8.com',
     'https://unpkg.com',
     'https://fonts.googleapis.com',
+    'https://nodei.co',
+    'https://www.google-analytics.com',
+    'https://www.googletagmanager.com',
+    'https://res.cloudinary.com',
+    'https://fonts.gstatic.com'
   ],
   styles: [
     'https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome-font-awesome.min.css',
     'https://unpkg.com/liser@stable/build/liser.min.css',
     'https://fonts.googleapis.com/css?family=Roboto:100,300,400',
-    'https://fonts.googleapis.com/css?family=Roboto+Slab:300',
+    'https://fonts.googleapis.com/css?family=Roboto+Slab:300'
   ],
   scripts: [
     'https://www.googletagmanager.com/gtag/js?id=UA-108475565-1',
-    gtagScript,
-  ],
+    gtagScript
+  ]
 }
 
 const regURL = /^((https|http)(:\/\/))?((([a-zA-Z0-9]+).)?([a-zA-Z0-9]+).([a-zA-Z0-9]+))(\/(\S+))?$/i
@@ -34,28 +39,28 @@ const isURL = str => regURL.test(str)
 
 export default {
   getSiteProps: () => ({
-    title: 'lamhieu',
+    title: 'lamhieu'
   }),
   getRoutes: async () => [
     {
       path: '/',
-      component: 'src/containers/Home/Vi',
+      component: 'src/containers/Home/Vi'
     },
     {
       path: '/en',
-      component: 'src/containers/Home/En',
+      component: 'src/containers/Home/En'
     },
     {
       is404: true,
-      component: 'src/containers/Home/Vi',
-    },
+      component: 'src/containers/Home/Vi'
+    }
   ],
   siteRoot: 'https://lamhieu.info',
   paths: {
     src: 'src', // the source directory. Must include an index.js entry file.
     dist: 'build', // the production output directory.
     devDist: 'dist', // the development scratch directory.
-    public: 'public', // the public directory (files copied to dist during build)
+    public: 'public' // the public directory (files copied to dist during build)
   },
   Document: ({ Html, Head, Body, children }: any) => (
     <Html lang="vi-VN">
@@ -85,5 +90,5 @@ export default {
       </Head>
       <Body>{children}</Body>
     </Html>
-  ),
+  )
 }
